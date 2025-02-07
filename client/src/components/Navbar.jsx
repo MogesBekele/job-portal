c6d04dc72e6e0793c303d8c157da27ccf792d4f0
MogesBekele <mogesbekele32@gmail.com>
2025-02-07 14:38:16 +0300

    commited

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import {assets} from '../assets/assets'
-import { UserButton, useClerk, useUser } from '@clerk/clerk-react'
+import React from "react";
+import { assets } from "../assets/assets";
+import { UserButton, useClerk, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const { user } = useUser()
-  const {openSignIn} = useClerk()
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
   return (
-    <div className='shadow py-4'>
-      <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center '>
+    <div className="shadow py-4">
+      <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center ">
         <img src={assets.logo} alt="" />
-        <div className='flex gap-4 max-sm:text-xs'>
-          <button className='text-gray-600'>Recruiter Login</button>
-          <button onClick={e=>openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
-        </div>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link to={'/applications'}>Applied jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName + " " + user.lastName}</p>
+            <UserButton/>
+          </div>
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={(e) => openSignIn()}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+            >
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
