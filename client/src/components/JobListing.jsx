e148c088e4c47964ca68360f9ec2ba2afc887bf4
MogesBekele <mogesbekele32@gmail.com>
2025-02-08 12:54:07 +0300

    commited

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const JobListing = () => {
-  const {isSearched, searchFilter} = useContext(AppContext)
+  const { isSearched, searchFilter } = useContext(AppContext);
   return (
     <div>
-      
+      <div>
+        {isSearched &&
+          (searchFilter.title !== "" || searchFilter.location !== "") && (
+            <>
+              <h3>Current Search</h3>
+              <div>
+                {searchFilter.title && <span>{searchFilter.title}</span>}
+                {searchFilter.location && <span>{searchFilter.location}</span>}
+              </div>
+            </>
+          )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JobListing
+export default JobListing;
