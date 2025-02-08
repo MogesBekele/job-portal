287b5499865dd541a448ba07649a4ed298bf7901
MogesBekele <mogesbekele32@gmail.com>
2025-02-08 12:57:33 +0300

    added

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const JobListing = () => {
   const { isSearched, searchFilter } = useContext(AppContext);
