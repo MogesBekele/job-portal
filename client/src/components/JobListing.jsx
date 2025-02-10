a5e559fd84d6f56bf11ba20991ef5863b8bc4ea9
MogesBekele <mogesbekele32@gmail.com>
2025-02-10 18:48:48 +0300

    commited

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { assets, JobCategories } from "../assets/assets";
+import { assets, JobCategories, JobLocations } from "../assets/assets";
 
 const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
@@ -44,10 +44,10 @@ const JobListing = () => {
           </div>
                {/*location  filter */}
                <div className="max-lg:hidden">
-            <h4 className="font-medium text-lg py-4">Search by Categories</h4>
+            <h4 className="font-medium text-lg py-4">Search by Location</h4>
             <ul className="space-y-4 text-gray-600">
               {
-                JobCategories.map((category, index)=>(
+                JobLocations.map((category, index)=>(
                   <li className="flex gap-3 items-center" key={index}>
                   <input className="scale-125" type="checkbox" name="" id="" />
                   {category}
