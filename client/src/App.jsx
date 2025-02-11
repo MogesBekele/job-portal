2523202796e3732bf6808a0f5bf442ec82b78c57
MogesBekele <mogesbekele32@gmail.com>
2025-02-11 20:58:02 +0300

    footer design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Applications from './pages/Applications.jsx'
 import ApplyJob from './pages/ApplyJob.jsx'
+import Footer from './pages/Footer.jsx'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/applications' element={<Applications />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
