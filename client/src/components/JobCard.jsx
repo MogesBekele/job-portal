73f6e2ccfb04a58ffad5f2dacad4799740d576e2
MogesBekele <mogesbekele32@gmail.com>
2025-02-11 16:23:14 +0300

    commited

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -5,17 +5,17 @@ const JobCard = ({job}) => {
   return (
     <div className='border p-6 shadow rounded'>
       <div className='flex justify-between items-center'>
-        <img src={assets.company_icon} alt="" />
+        <img className='h-8' src={assets.company_icon} alt="" />
       </div>
-      <h4>{job.title}</h4>
-      <div>
-        <span>{job.location}</span>
-        <span>{job.label}</span>
+      <h4 className='font-medium text-xl mt-2'>{job.title}</h4>
+      <div className='flex items-center gap-3 mt-2 text-sm'>
+        <span className='bg-blue-50 border-blue-200 px-4 py-1.5 rounded'>{job.location}</span>
+        <span className='bg-red-50 border-red-200 px-4 py-1.5 rounded'>{job.level}</span>
       </div>
-      <p dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
-      <div>
-        <button>Apply now</button>
-        <button>Learn mode</button>
+      <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
+      <div className='mt-4 flex gap-4 text-sm'>
+        <button className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
+        <button className='text-gray-500 border border-gray-500 rounded px-4 py-2 '>Learn mode</button>
       </div>
     </div>
   )
