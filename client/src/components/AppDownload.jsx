b3f3fe9a3996b460956aea1840b2dad20d7644ee
MogesBekele <mogesbekele32@gmail.com>
2025-02-12 17:37:21 +0300

    commited

--- client/src/components/AppDownload.jsx
+++ client/src/components/AppDownload.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AppDownload = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h1>Download Mobile App For Better Experience</h1>
+          <div>
+            <a href="">
+              <img src={assets.play_store} alt="" />
+            </a>
+            <a href="">
+              <img src={assets.app_store} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
