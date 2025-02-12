bf3d21d59a4268d5173617bc7180fb7decd0f865
MogesBekele <mogesbekele32@gmail.com>
2025-02-12 18:04:54 +0300

    commited

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center py-3 gap-4 mt-20'>
       <img src={assets.logo} alt="" />
-      <p>Copyright @MogesBekele | All right reserved</p>
-      <div>
+      <p className='flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @MogesBekele | All right reserved</p>
+      <div className='flex gap-2.5'>
         <img width={38}  src={assets.facebook_icon} alt="" />
         <img width={38}  src={assets.twitter_icon} alt="" />
         <img width={38} src={assets.instagram_icon} alt="" />
