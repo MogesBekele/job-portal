baac72a75db9451f7df9596a9fd92b105e848c67
MogesBekele <mogesbekele32@gmail.com>
2025-02-12 17:09:44 +0300

    pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -86,7 +86,7 @@ const JobListing = () => {
         </h3>
         <p className="mb-8">Get your desired jobs from top companies</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-          {jobs.map((job, index) => (
+          {jobs.slice((currentPage-1)*6, currentPage*6).map((job, index) => (
             <JobCard key={index} job={job} />
           ))}
         </div>
