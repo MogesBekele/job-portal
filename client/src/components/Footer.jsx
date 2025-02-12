aa45fab416d0b8045e80e58de91d76c8cc0d3a74
MogesBekele <mogesbekele32@gmail.com>
2025-02-12 17:56:47 +0300

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <img src={assets.logo} alt="" />
+      <p>Copyright @MogesBekele | All right reserved</p>
+      <div>
+        <img width={38}  src={assets.facebook_icon} alt="" />
+        <img width={38}  src={assets.twitter_icon} alt="" />
+        <img width={38} src={assets.instagram_icon} alt="" />
+      </div>
     </div>
   )
 }
