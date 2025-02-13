8c7112c8e8e0770a28e3d7f838c1929e1691198d
MogesBekele <mogesbekele32@gmail.com>
2025-02-13 14:53:32 +0300

    changed

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -9,6 +9,7 @@ const JobListing = () => {
     useContext(AppContext);
   const [showFilter, setShowFilter] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategories, setSelectedCategories] =useState([])
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       <div className="w-full lg:w-1/4 bg-white px-4">
