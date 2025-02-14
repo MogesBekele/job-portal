bdbb3e618d74ddf5e037abb508669cc094117c59
MogesBekele <mogesbekele32@gmail.com>
2025-02-14 15:54:24 +0300

    commit

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const ApplyJob = () => {
+
+  const {id} = useParams()
+  const [jobData, setJobData] = useState(null)
   return (
     <div>
       
