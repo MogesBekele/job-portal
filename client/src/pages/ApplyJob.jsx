c3b0d226b56cfe9259180f00e632aefd3b0655e3
MogesBekele <mogesbekele32@gmail.com>
2025-02-14 16:12:12 +0300

    commited

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -26,7 +26,9 @@ if(data.length !== 0){
       
     </div>
   ):(
-    <div></div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='w-20 h-20 border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin ' ></div>
+    </div>
   )
 }
 
