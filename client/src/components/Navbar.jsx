8ea77db12f50790b0876f167ca2949ca550dc116
MogesBekele <mogesbekele32@gmail.com>
2025-02-15 12:40:43 +0300

    commited

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { UserButton, useClerk, useUser } from "@clerk/clerk-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user } = useUser();
   const { openSignIn } = useClerk();
+  const navigate = useNavigate()
 
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center ">
-        <img src={assets.logo} alt="" />
+        <img onClick={()=>navigate('/')}  src={assets.logo} alt="" />
         {user ? (
           <div className="flex items-center gap-3">
             <Link to={'/applications'}>Applied jobs</Link>
