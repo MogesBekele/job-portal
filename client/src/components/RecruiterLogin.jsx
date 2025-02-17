efa5d2508ee399c113bf57d218856414fc45acee
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 21:41:11 +0300

    img

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const RecruiterLogin = () => {
   const [state, setState] =useState('login')
@@ -12,6 +13,11 @@ const RecruiterLogin = () => {
       <form action="">
         <h1>Recruiter {state}</h1>
         <p>welcome back! please sign in to continue</p>
+        <>
+        <div>
+          <img src={assets.person_icon} alt="" />
+        </div>
+        </>
       </form>
     </div>
   )
