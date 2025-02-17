ae2023b5561e196dc7fd3581b72abe344bf58ede
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 22:09:56 +0300

    commit

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -15,10 +15,11 @@ const RecruiterLogin = () => {
         <p className='text-sm'>welcome back! please sign in to continue</p>
       
         <>
-        <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
+        {state !== 'Login' && (   <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.person_icon} alt="" />
           <input className='outline-none text-sm' onChange={e=>setName(e.target.value)} value={name} type="text" placeholder='Company Name' required />
-        </div>
+        </div>)}
+     
         <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.email_icon} alt="" />
           <input  className='outline-none text-sm'  onChange={e=>setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
