8133abc6ede36f878e1e5c12ef92767a5773c28a
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 21:57:59 +0300

    cimmit

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -12,7 +12,7 @@ const RecruiterLogin = () => {
     <div className='absolute top-0 right-0 bottom-0 left-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       <form className='relative bg-white p-10 rounded-xl text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state}</h1>
-        <p>welcome back! please sign in to continue</p>
+        <p className='text-sm'>welcome back! please sign in to continue</p>
         <>
         <div>
           <img src={assets.person_icon} alt="" />
