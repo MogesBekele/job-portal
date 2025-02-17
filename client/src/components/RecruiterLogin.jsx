f8c9404576cc03c46ce9ae34a1b9e9c1a823e9d1
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 22:00:00 +0300

    commit

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -18,7 +18,7 @@ const RecruiterLogin = () => {
           <img src={assets.person_icon} alt="" />
           <input onChange={e=>setName(e.target.value)} value={name} type="text" placeholder='Company Name' required />
         </div>
-        <div>
+        <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.email_icon} alt="" />
           <input onChange={e=>setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
         </div>
