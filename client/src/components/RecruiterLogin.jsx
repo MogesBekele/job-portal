127ac342b864a147f25cd82c12deea8408a3c715
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 21:59:13 +0300

    commit

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -14,7 +14,7 @@ const RecruiterLogin = () => {
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state}</h1>
         <p className='text-sm'>welcome back! please sign in to continue</p>
         <>
-        <div>
+        <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.person_icon} alt="" />
           <input onChange={e=>setName(e.target.value)} value={name} type="text" placeholder='Company Name' required />
         </div>
