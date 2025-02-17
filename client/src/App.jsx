290e706b1ab2cecae31a41dd5e6d86d34cc3ccb4
MogesBekele <mogesbekele32@gmail.com>
2025-02-17 21:31:34 +0300

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Applications from './pages/Applications.jsx'
 import ApplyJob from './pages/ApplyJob.jsx'
 import RecruiterLogin from './components/RecruiterLogin.jsx'
+import { AppContext } from './context/AppContext.jsx'
 
 
 const App = () => {
+  const [showRecruiterLogin] = useContext(AppContext)
   return (
     <div >
-      <RecruiterLogin/>
+      { showRecruiterLogin && <RecruiterLogin/>}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/applications' element={<Applications />} />
