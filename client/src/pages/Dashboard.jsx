7db962b43d2115edadfbd38f9c43b391763456d1
MogesBekele <mogesbekele32@gmail.com>
2025-02-18 17:35:37 +0300

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,9 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>helloo</h2>
-      <p>all in one</p>
+    <div className='min-h-screen'>
+     
     
     </div>
   )
