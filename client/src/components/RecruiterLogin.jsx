4a2f7de71c0260fdc6efc08d3e379d1f90246044
MogesBekele <mogesbekele32@gmail.com>
2025-02-18 15:53:58 +0300

    commit

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -25,7 +25,7 @@ const RecruiterLogin = () => {
           ?<>
           <div className='flex items-center gap-4 my-10'>
             <label htmlFor="image">
-              <img className='w-16 rounded-full' src={assets.upload_area} alt="" />
+              <img className='w-16 rounded-full' src={ image? URL.createObjectURL(image): assets.upload_area} alt="" />
               <input onChange={e=>setImage(e.target.files[0])} type="file" id='image' hidden />
             </label>
             <p>upload company <br /> logo</p>
