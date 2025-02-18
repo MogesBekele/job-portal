7c2d46d14bfba0cb0faa2805222e953ab8f50c39
MogesBekele <mogesbekele32@gmail.com>
2025-02-18 17:36:14 +0300

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div className='min-h-screen'>
+      {/* navbar for recruiter panel */}
      
     
     </div>
