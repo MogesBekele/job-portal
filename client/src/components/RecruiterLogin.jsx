88f29ba84a53be4ec959fa6104628b11b1c9349f
MogesBekele <mogesbekele32@gmail.com>
2025-02-18 15:34:58 +0300

    function

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -10,6 +10,9 @@ const RecruiterLogin = () => {
   const [isTextDataSubmited, setIsTextDataSubmited] =useState(false)
   const onSubmitHandler = async(e)=>{
     e.preventDefault
+    if (state == 'sign up' && !isTextDataSubmited) {
+      
+    }
   }
   return (
     <div className='absolute top-0 right-0 bottom-0 left-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
