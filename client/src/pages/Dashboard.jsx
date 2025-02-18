d9bf689d96f2b9d51ac6942c1681243980931987
MogesBekele <mogesbekele32@gmail.com>
2025-02-18 17:46:37 +0300

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -12,8 +12,8 @@ const Dashboard = () => {
           <img className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
           <div className='flex items-center gap-3'>
             <p className='max-sm:hidden'>Welcome, Job board</p>
-            <div>
-              <img src={assets.company_icon} alt="" />
+            <div className='relative group'>
+              <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
               <div>
                 <ul>
                   <li>Logout</li>
