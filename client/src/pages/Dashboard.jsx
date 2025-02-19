abed180568ece2f21e0a1b10727b3d28295b378e
MogesBekele <mogesbekele32@gmail.com>
2025-02-19 15:17:24 +0300

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
             <p className='max-sm:hidden'>Welcome, Job board</p>
             <div className='relative group'>
               <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
-              <div>
+              <div className='absolute hidden group-hover:block top-0 right-0'>
                 <ul>
                   <li>Logout</li>
                 </ul>
