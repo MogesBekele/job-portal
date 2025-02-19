0e7ccba65a93ca86e8c7c4556dcd50505eb107c2
MogesBekele <mogesbekele32@gmail.com>
2025-02-19 17:23:22 +0300

    commit

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 
 const AddJob = () => {
   const [title, setTitle] = useState('')
+  const [location, setLocation] = useState('addis')
+  const [category, setCategory] = useState('programming')
+  const [level, setLevel] = useState('Beginner level')
+  const [salary, setSalary] =useState(0)
   return (
-    <div>
-      
-    </div>
+    <form>
+      <div>
+        <p>Job Title</p>
+        <input type="text" placeholder='type here' onChange={e=>setTitle(e.target.value)} value={title}/>
+      </div>
+    </form>
   )
 }
 
