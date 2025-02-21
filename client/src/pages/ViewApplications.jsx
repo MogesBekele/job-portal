0da0927942419607fd793b431c24f065abbb8b6d
MogesBekele <mogesbekele32@gmail.com>
2025-02-21 20:48:47 +0300

    commit

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -36,7 +36,7 @@ const ViewApplications = () => {
                 <div className='relative inline-block text-left group'>
                   <button className='text-gray-500 action-button'>...</button>
              
-                <div className='z-10 hidden absolute right-0 mt-2 w-32 top-0 bg-white border border-gray-200 rounded shadow group-hover:block'>
+                <div className='z-10 hidden absolute right-0 mt-2 w-32 md:left-0 top-0 bg-white border border-gray-200 rounded shadow group-hover:block'>
                   <button className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>Accept</button>
                   <button  className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>Reject</button>
                 </div>
