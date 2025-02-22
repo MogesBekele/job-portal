2badcfd659d9c60a0fcf780589294414595523be
MogesBekele <mogesbekele32@gmail.com>
2025-02-22 20:41:54 +0300

    commit

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import { manageJobsData } from '../assets/assets'
-import moment from 'moment'
+import React from "react";
+import { manageJobsData } from "../assets/assets";
+import moment from "moment";
 
 const ManageJobs = () => {
   return (
-    <div>
-   <div>
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Job Title</th>
-          <th>Date</th>
-          <th>Location</th>
-          <th>Applicants</th>
-          <th>Visible</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          manageJobsData.map((job, index)=>(
-            <tr key={index}>
-              <td>{index+1}</td>
-              <td>{job.title}</td>
-              <td>{moment(job.date).format('ll')}</td>
-              <td>{job.location}</td>
-              <td>{job.applicants}</td>
-              <td>
-                <input type="checkbox" />
-              </td>
+    <div className="container p-4 max-w-5xl">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Job Title</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Applicants</th>
+              <th>Visible</th>
             </tr>
-          ))
-        }
-      </tbody>
-    </table>
-   </div>
+          </thead>
+          <tbody>
+            {manageJobsData.map((job, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{job.title}</td>
+                <td>{moment(job.date).format("ll")}</td>
+                <td>{job.location}</td>
+                <td>{job.applicants}</td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageJobs
+export default ManageJobs;
