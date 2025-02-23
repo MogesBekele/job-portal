9c16bb3fd1188d70d6df2aa596bf8966258698c2
MogesBekele <mogesbekele32@gmail.com>
2025-02-23 12:14:14 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(cors());
