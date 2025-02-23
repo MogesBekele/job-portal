b2db3b59b90eab0e30e8b6677dbd805d7963c13a
MogesBekele <mogesbekele32@gmail.com>
2025-02-23 13:24:27 +0300

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 mongoose.connection.on("connected", () => {
 console.log("MongoDB connected");});
-await mongoose.connect(`${process.env.MONGODB_URI}`)
+await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
 }
 
 export default connectDB;
