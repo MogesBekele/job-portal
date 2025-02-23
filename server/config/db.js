f7b4651f9ff7185777daf342c44d9ccc8c308407
MogesBekele <mogesbekele32@gmail.com>
2025-02-23 16:27:59 +0300

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit process with failure
-  }
-};
-
-mongoose.connection.on("error", (err) => {
-  console.error(`MongoDB connection error: ${err}`);
-});
+mongoose.connection.on("connected", () => {
+  
+console.log("MongoDB connected");});
+await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
 
 export default connectDB;
