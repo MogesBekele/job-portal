0efbbae86b874604755b08a423cd69967768bd61
MogesBekele <mogesbekele32@gmail.com>
2025-02-23 12:45:06 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/db.js';
 
 const app = express();
+//connect to database
+await connectDB()
 const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
