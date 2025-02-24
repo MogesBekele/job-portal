a2c5548d58ae973f13fa96a26b0e15c049601d28
MogesBekele <mogesbekele32@gmail.com>
2025-02-24 20:57:10 +0300

    commit

--- server/controller/Webhook.js
+++ server/controller/Webhook.js
@@ -6,6 +6,13 @@ export const clerkWebhooks = async(req, res) => {
   try{
  // CREATE A svix instance with clerk webhook secret
  const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+  // VERIFY THE headers
+
+  await whook.verify(JSON.stringify(req.body),{
+    "svix-id": req.headers["svix-id"],
+    "svix-timestamp" : req.headers["svix-timestamp"],
+    "svix-signature" : req.headers["svix-signature"]
+  });
 
   }
   catch(error){
