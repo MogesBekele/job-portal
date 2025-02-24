1ad25fec15d99a570f5e5357422a5c6b8c82169f
MogesBekele <mogesbekele32@gmail.com>
2025-02-24 20:48:17 +0300

    commit

--- server/controller/Webhook.js
+++ server/controller/Webhook.js
@@ -2,3 +2,11 @@ import { Webhook } from "svix";
 import User from "../models/User";
 
 // API CONTROLLER fun to manage clerk user with database
+export const clerkWebhooks = async(req, res) => {
+  try{
+
+  }
+  catch(error){
+    console.log(error);
+  }
+}
