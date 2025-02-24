322077a7596268def8e2504c7274fd0c93a9e17f
MogesBekele <mogesbekele32@gmail.com>
2025-02-24 20:19:40 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import './config/instrument.js'
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/db.js';
+import "./config/instrument.js";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,12 +13,13 @@ app.use(express.json());
 
 await connectDB();
 
-
-
-
 // Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, api!');
+app.get("/", (req, res) => {
+  res.send("Hello, api!");
 });
 
 // Function to connect to the database with retries
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
