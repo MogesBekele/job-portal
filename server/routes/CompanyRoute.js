507aa84ae6475b6e25ed80c2eb50083efb0cf835
MogesBekele <mogesbekele32@gmail.com>
2025-02-25 20:53:32 +0300

    commit

--- server/routes/CompanyRoute.js
+++ server/routes/CompanyRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-
+import { registerCompany } from '../controllers/CompanyController.js';
 const router = express.Router();
 
 
 //Register a company
 
 router.post('/register, registerCompany');
+
+//Login a company
+
+router.post('/login', loginCompany);
