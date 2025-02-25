1cb6197fe19d8f2bb4454a3f6217f17491e67a82
MogesBekele <mogesbekele32@gmail.com>
2025-02-25 20:43:01 +0300

    commit

--- server/controller/CompanyController.js
+++ server/controller/CompanyController.js
@@ -8,5 +8,16 @@ export const registerCompany = async (req, res) => {
 //company login
 
 export const loginCompany = async (req, res) => {
-  
+
 }
+
+//get company data
+
+export const getCompanyData = async (req, res) => {}
+
+//post job
+
+export const postJob = async (req, res) => {}
+
+//get all job applicants
+
