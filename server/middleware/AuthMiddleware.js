3c23d72789cbc7c1424f4f1dc2d98bd972a768cf
MogesBekele <mogesbekele32@gmail.com>
2025-02-26 17:56:36 +0300

    commit

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -1,8 +1,7 @@
 import jwt from 'jsonwebtoken';
-import Company from '../models/Company';
+import Company from '../models/Company.js';
 
 export const protectCompany = async (req, res, next) => {
-
   const token = req.headers.token;
 
   if (!token) {
@@ -12,12 +11,10 @@ export const protectCompany = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const  req.company  = await Company.findById(decoded.id.select('-password'));
+    req.company = await Company.findById(decoded.id).select('-password');
 
     next();
   } catch (error) {
     return res.json({ success: false, message: 'Not authorized to access this route' });
   }
- 
-
-}
+};
