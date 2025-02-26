3b499516e6aa6515163727374fbf43fb3f818e9d
MogesBekele <mogesbekele32@gmail.com>
2025-02-26 17:49:26 +0300

    commit

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -3,23 +3,25 @@ import Company from '../models/Company';
 
 export const protectCompany = async (req, res, next) => {
 
-  let token;
-
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.company = await Company.findById(decoded.id).select('-password');
-
-      next();
-    } catch (error) {
-      return res.json({ success: false, message: 'Not authorized to access this route' });
-    }
-  }
+  const token = req.headers.token;
 
   if (!token) {
+    return res.json({ success: false, message: 'Not authorized to access this route' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const company = await Company.findById(decoded.id);
+
+    if (!company) {
+      return res.json({ success: false, message: 'No company found with this id' });
+    }
+
+    req.company = company;
+
+    next();
+  } catch (error) {
     return res.json({ success: false, message: 'Not authorized to access this route' });
   }
 
