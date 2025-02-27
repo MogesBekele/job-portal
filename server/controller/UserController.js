f052857519eec18e0ee8b7e57a672150a7fbb90c
MogesBekele <mogesbekele32@gmail.com>
2025-02-27 10:58:42 +0300

    commit

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -10,5 +10,13 @@ export const getUserData = async (req, res) => {
 //apply for a job
 
 export const applyForJob = async (req, res) => {
-  
+
 }
+
+//get user applied applications
+
+export const getUserJobApplications = async (req, res) => {}
+
+//update user profile
+
+export const updateUserResume = async (req, res) => {}
