9adff0a4f5515f09f7b313dca20329b0084dac70
MogesBekele <mogesbekele32@gmail.com>
2025-02-27 10:14:58 +0300

    commit

--- server/routes/JobsRoute.js
+++ server/routes/JobsRoute.js
@@ -3,6 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 // all jobs data
+router.get('/', (req, res) => {
+  res.send('All jobs data')
+}
 
 // route to get a single job by id
 
