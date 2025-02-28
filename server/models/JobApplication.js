84842d580a6b14e1eb45bd1fc5278305b1322ef0
MogesBekele <mogesbekele32@gmail.com>
2025-02-28 09:17:47 +0300

    commit

--- server/models/JobApplication.js
+++ server/models/JobApplication.js
@@ -3,5 +3,6 @@ import mongoose from "mongoose";
 
 const JobApplicationSchema = new mongoose.Schema({
 
-  userId: {type: String, required: true},
+  userId: {type: String, ref: 'User',  required: true},
+  companyId: {type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true},
 })
