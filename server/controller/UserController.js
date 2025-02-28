c35dc7a91f1164d8ec9cfa86aea1f7dab1ca1139
MogesBekele <mogesbekele32@gmail.com>
2025-02-28 09:13:21 +0300

    commit

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,22 +1,40 @@
-
+import User from "../models/User.js";
 
 // get user data
 
 export const getUserData = async (req, res) => {
+  const userId = req.auth.userId;
 
+  try {
+    const user = await User.findById(userId);
 
-}
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.json({
+      success: true,
+      user: user,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //apply for a job
 
-export const applyForJob = async (req, res) => {
-
-}
+export const applyForJob = async (req, res) => {};
 
 //get user applied applications
 
-export const getUserJobApplications = async (req, res) => {}
+export const getUserJobApplications = async (req, res) => {};
 
 //update user profile
 
-export const updateUserResume = async (req, res) => {}
+export const updateUserResume = async (req, res) => {};
