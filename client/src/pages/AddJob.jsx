9571623c55975f99e113fe594b702a22da3ca2eb
MogesBekele <mogesbekele32@gmail.com>
2025-03-01 15:34:10 +0300

    com

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -9,19 +9,17 @@ const AddJob = () => {
   const [location, setLocation] = useState('addis')
   const [category, setCategory] = useState('programming')
   const [level, setLevel] = useState('Beginner level')
-  const [salary, setSalary] =useState(0)
+  const [salary, setSalary] = useState(0)
   const editorRef = useRef(null)
   const quillRef = useRef(null)
-  const {backEndUrl, companyToken} = useContext(AppContext)
+  const { backEndUrl, companyToken } = useContext(AppContext)
 
-
-  const onSubmitHandler = async (e)=>{
-    e.preventDefualt()
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
     try {
-
       const description = quillRef.current.root.innerHTML
-      const {data} = await axios.post(backEndUrl + '/api/company/post-job', {title, description, location, category, level},
-        {headers: {token: companyToken}}
+      const { data } = await axios.post(backEndUrl + '/api/company/post-job', { title, description, location, category, level, salary },
+        { headers: { token: companyToken } }
       )
 
       if (data.success) {
@@ -29,78 +27,67 @@ const AddJob = () => {
         setTitle('')
         setSalary(0)
         quillRef.current.root.innerHTML = ""
-
-      }
-      else{
+      } else {
         toast.error(data.message)
       }
-      
     } catch (error) {
-
       toast.error(error.message)
-      
     }
-
-
   }
 
-  useEffect(()=>{
-       //initiate quill only once
-
-     if (!quillRef.current && editorRef.current) {
+  useEffect(() => {
+    //initiate quill only once
+    if (!quillRef.current && editorRef.current) {
       quillRef.current = new Quill(editorRef.current, {
         theme: 'snow',
       })
-      
-     }
-  },[])
+    }
+  }, [])
+
   return (
     <form onSubmit={onSubmitHandler} className='container p-4 flex flex-col w-full items-start gap-3 '>
       <div className='w-full'>
         <p className='mb-2'>Job Title</p>
-        <input type="text" placeholder='type here' onChange={e=>setTitle(e.target.value)} value={title} required
-        className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded'/>
+        <input type="text" placeholder='type here' onChange={e => setTitle(e.target.value)} value={title} required
+          className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded' />
       </div>
       <div className='w-full max-w-lg'>
         <p className='my-2'>Job Description</p>
-        <div ref={editorRef}>
-
-        </div>
+        <div ref={editorRef}></div>
       </div>
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
           <p className='mb-2'>Job Category</p>
-          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e=>setCategory(e.target.value)}>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e => setCategory(e.target.value)}>
             {
-             JobCategories.map((category, index)=>(
-              <option key={index} value={category}>{category}</option>
-             ))
+              JobCategories.map((category, index) => (
+                <option key={index} value={category}>{category}</option>
+              ))
             }
           </select>
         </div>
         <div>
           <p className='mb-2'>Job Location</p>
-          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e=>setLocation(e.target.value)}>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e => setLocation(e.target.value)}>
             {
-             JobLocations.map((location, index)=>(
-              <option key={index} value={location}>{location}</option>
-             ))
+              JobLocations.map((location, index) => (
+                <option key={index} value={location}>{location}</option>
+              ))
             }
           </select>
         </div>
         <div>
           <p className='mb-2'>Job Level</p>
-          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e=>setLevel(e.target.value)}>
-          <option value="Beginner level">Beginner level</option>
-          <option value="Intermediate leve">Intermediate level</option>
-          <option value="Senior level">Senior level</option>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded ' onChange={e => setLevel(e.target.value)}>
+            <option value="Beginner level">Beginner level</option>
+            <option value="Intermediate level">Intermediate level</option>
+            <option value="Senior level">Senior level</option>
           </select>
         </div>
-
       </div>
       <div>
         <p className='mb-2'>Job Salary</p>
-        <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px] ' onChange={e=>setSalary(e.target.value)} type="Number" placeholder='2500'/>
+        <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px] ' onChange={e => setSalary(e.target.value)} type="Number" placeholder='2500' />
       </div>
       <button className='w-28 py-3 mt-4 bg-black text-white cursor-pointer rounded'>ADD</button>
     </form>
