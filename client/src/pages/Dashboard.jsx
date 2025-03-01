0d3c77123afbb5171ce41bce62091ec8b7ac9ff6
MogesBekele <mogesbekele32@gmail.com>
2025-03-01 13:40:10 +0300

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import { AppContext } from "../context/AppContext";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const {companyData} = useContext(AppContext)
+  const { companyData } = useContext(AppContext);
   return (
     <div className="min-h-screen">
       {/* navbar for recruiter panel */}
@@ -18,27 +18,23 @@ const Dashboard = () => {
             src={assets.logo}
             alt=""
           />
-          {
-            companyData? <>
-                  <div className="flex items-center gap-3">
-            <p className="max-sm:hidden">Welcome, Job board</p>
-            <div className="relative group">
-              <img
-                className="w-8  rounded-full cursor-pointer"
-                src={assets.company_icon}
-                alt=""
-              />
-              <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
-                <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
-                  <li className="py-1 px-2 pr-10 cursor-pointer">Logout</li>
-                </ul>
+          {companyData && (
+            <div className="flex items-center gap-3">
+              <p className="max-sm:hidden">Welcome, Job board</p>
+              <div className="relative group">
+                <img
+                  className="w-8  rounded-full cursor-pointer"
+                  src={assets.company_icon}
+                  alt=""
+                />
+                <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
+                  <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
+                    <li className="py-1 px-2 pr-10 cursor-pointer">Logout</li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-            
-            </>: null
-          }
-    
+          )}
         </div>
       </div>
 
@@ -47,22 +43,43 @@ const Dashboard = () => {
 
         <div className="inline-block min-h-screen border-r-1">
           <ul className="flex flex-col items-start pt-5 text-gray-800">
-            <NavLink className={({isActive})=>` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/add-job"}>
+            <NavLink
+              className={({ isActive }) =>
+                ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/add-job"}
+            >
               <img className="min-w-4" src={assets.add_icon} alt="" />
               <p className="max-sm:hidden">Add Job</p>
             </NavLink>
-            <NavLink className={({isActive})=>` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/manage-jobs"}>
+            <NavLink
+              className={({ isActive }) =>
+                ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/manage-jobs"}
+            >
               <img className="min-w-4" src={assets.home_icon} alt="" />
               <p className="max-sm:hidden">Manage Jobs</p>
             </NavLink>
-            <NavLink className={({isActive})=>` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/view-applications"}>
+            <NavLink
+              className={({ isActive }) =>
+                ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/view-applications"}
+            >
               <img className="min-w-4" src={assets.person_tick_icon} alt="" />
               <p className="max-sm:hidden">View Applications</p>
             </NavLink>
           </ul>
         </div>
         <div>
-          <Outlet/>
+          <Outlet />
         </div>
       </div>
     </div>
