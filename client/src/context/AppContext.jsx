a06fa8c4aef0d620c7639153cadbac7b14e5c53a
MogesBekele <mogesbekele32@gmail.com>
2025-03-01 13:19:39 +0300

    commit

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,53 +1,66 @@
-import { createContext, useEffect } from 'react';
-import { useState } from 'react';
-import JobCard from '../components/JobCard';
-import { jobsData } from '../assets/assets';
+import { createContext, useEffect } from "react";
+import { useState } from "react";
+import JobCard from "../components/JobCard";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
-export const AppContextProvider = (props)=>{
-  const backEndUrl =  import.meta.env.VITE_BACKEND_URL
+export const AppContextProvider = (props) => {
+  const backEndUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [searchFilter, setSearchFilter] = useState({
-    title: '',
-    location: ''
-  })
-  const [jobs, setJobs] = useState([])
-  const [isSearched, setIsSearched] = useState(false)
+    title: "",
+    location: "",
+  });
+  const [jobs, setJobs] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
 
-  const [showRecruiterLogin, setShowRecruiterLogin] =useState(false)
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
 
-  const [companyToken, setCompanyToken] = useState(null)
-  const [companyData, setCompanyData] = useState(null)
+  const [companyToken, setCompanyToken] = useState(null);
+  const [companyData, setCompanyData] = useState(null);
 
-  const fetchJobs = async ()=>{
-    setJobs(jobsData)
-  }
-  useEffect(()=>{
-    fetchJobs()
+  const fetchJobs = async () => {
+    setJobs(jobsData);
+  };
 
-    const storeCompanyToken = localStorage.getItem('companyToken')
-    if(storeCompanyToken){
-      setCompanyToken(storeCompanyToken)
+  //fetch company data
+
+  const fetchCompanyData = async () => {
+    try {
+      const { data } = await axios.get(backEndUrl + "/api/company/company", {
+        headers: {
+          token: companyToken,
+        },
+      });
+      setCompanyData(data.company);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchJobs();
+
+    const storeCompanyToken = localStorage.getItem("companyToken");
+    if (storeCompanyToken) {
+      setCompanyToken(storeCompanyToken);
     }
-    
-  },[])
+  }, []);
   const value = {
     backEndUrl,
     searchFilter,
     setSearchFilter,
     isSearched,
     setIsSearched,
-    jobs, setJobs,
+    jobs,
+    setJobs,
     showRecruiterLogin,
-   setShowRecruiterLogin,
-    companyToken, setCompanyToken,
-    companyData, setCompanyData
-
-  }
+    setShowRecruiterLogin,
+    companyToken,
+    setCompanyToken,
+    companyData,
+    setCompanyData,
+  };
   return (
-    <AppContext.Provider value={value}>
-      {props.children}
-    </AppContext.Provider>
-  )
-}
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
