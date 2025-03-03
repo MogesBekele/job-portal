b6b402f2695ddaca03ff81e9cb484622d4370fac
MogesBekele <mogesbekele32@gmail.com>
2025-03-03 14:07:43 +0300

    commit

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -92,7 +92,7 @@ const ApplyJob = () => {
               </div>
             </div>
             <div className="flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center ">
-              <button className="bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer">
+              <button onClick={applyHandler} className="bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer">
                 Apply Now
               </button>
               <p className="mt-1 text-gray-600">
