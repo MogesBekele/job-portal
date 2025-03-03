4dcb687ae4f272cbacd5a6865efc8cb79d078bf4
MogesBekele <mogesbekele32@gmail.com>
2025-03-03 14:57:17 +0300

    commit

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -83,7 +83,7 @@ const updateResume= async ()=>{
           <tbody>
             {
               userApplications.map((job, index)=>true ?(
-                <tr>
+                <tr key={index}>
                   <td className='py-3 px-4 flex items-center gap-2 border-b'>
                     <img className='w-8 h-8' src={job.companyId.image} alt="" />
                     {job.companyId.name}
