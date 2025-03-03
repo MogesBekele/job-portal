8d680811ca948c786bbd008c245b5ec86983adef
MogesBekele <mogesbekele32@gmail.com>
2025-03-03 14:29:55 +0300

    commit

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -58,7 +58,7 @@ const updateResume= async ()=>{
               <input id='resumeUpload' onChange={e=>setResume(e.target.files[0])} accept='application/pdf' type="file" hidden />
               <img src={assets.profile_upload_icon} alt="" />
             </label>
-            <button onClick={e=>setIsEdit(false)} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
+            <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
             </>
             : <div className='flex gap-2'>
               <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
