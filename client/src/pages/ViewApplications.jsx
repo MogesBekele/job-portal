628a6b3f340ff98cb3af49dd6b0a6b097ecb1107
MogesBekele <mogesbekele32@gmail.com>
2025-03-04 16:59:38 +0300

    commit

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -102,10 +102,10 @@ const ViewApplications = () => {
                     <button className="text-gray-500 action-button">...</button>
 
                     <div className="z-10 hidden absolute right-0 mt-2 w-32 md:left-0 top-0 bg-white border border-gray-200 rounded shadow group-hover:block">
-                      <button className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">
+                      <button onClick={()=>changeJobApplicationStatus(applicant._id, 'Accepted')} className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">
                         Accept
                       </button>
-                      <button className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">
+                      <button onClick={()=>changeJobApplicationStatus(applicant._id, 'Rejected')} className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">
                         Reject
                       </button>
                     </div>
