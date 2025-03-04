0ff9bd61c98434fa600d12d439e27b8b71e9ce90
MogesBekele <mogesbekele32@gmail.com>
2025-03-04 20:21:06 +0300

    commit

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -82,9 +82,9 @@ const Applications = () => {
             </>
           ) : (
             <div className="flex gap-2">
-              <a
+              <a target="_blank" href={userData.resume}
                 className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg"
-                href=""
+                
               >
                 Resume
               </a>
