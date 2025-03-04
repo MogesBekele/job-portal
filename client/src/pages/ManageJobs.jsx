45a5df9cff8fd99717b69f620c765dd8739b191a
MogesBekele <mogesbekele32@gmail.com>
2025-03-04 17:22:11 +0300

    commit

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -24,7 +24,6 @@ const ManageJobs = () => {
       if (data.success) {
         setJobs(data.jobsData.reverse());
         console.log(data.jobsData);
-        
       } else {
         toast.error(data.message);
       }
@@ -33,11 +32,10 @@ const ManageJobs = () => {
     }
   };
 
-  //to change visibility 
+  //to change visibility
 
   const changeJobVisibility = async (id) => {
     try {
-
       const { data } = await axios.post(
         backEndUrl + "/api/company/change-visibility",
         { id },
@@ -47,83 +45,87 @@ const ManageJobs = () => {
       if (data.success) {
         toast.success(data.message);
         fetchCompanyJobs();
-        
-      }else{
+      } else {
         toast.error(data.message);
       }
-
-      
     } catch (error) {
       toast.error(error.message);
-      
     }
-   
-    }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (companyToken) {
-      fetchCompanyJobs()
-      
+      fetchCompanyJobs();
     }
+  }, [companyToken]);
 
-  }, [companyToken])
-
-  return jobs ? jobs.length === 0 ? (
-  
-  <div className="fle items-center justify-center h-[70vh]">
-    <p className="text-xl sm:text-2xl">No Jobs Available</p>
-  </div>): (
-    <div className="container p-4 max-w-5xl">
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 border-b text-left max-sm:hidden">#</th>
-              <th className="px-4 py-2 border-b text-left">Job Title</th>
-              <th className="px-4 py-2 border-b text-left max-sm:hidden">
-                Date
-              </th>
-              <th className="px-4 py-2 border-b text-left max-sm:hidden">
-                Location
-              </th>
-              <th className="px-4 py-2 border-b text-center">Applicants</th>
-              <th className="px-4 py-2 border-b text-left">Visible</th>
-            </tr>
-          </thead>
-          <tbody>
-            {jobs.map((job, index) => (
-              <tr key={index} className="text-gray-700">
-                <td className="py-2 px-4 border-b max-sm:hidden">
-                  {index + 1}
-                </td>
-                <td className="py-2 px-4 border-b">{job.title}</td>
-                <td className="py-2 px-4 border-b max-sm:hidden">
-                  {moment(job.date).format("ll")}
-                </td>
-                <td className="py-2 px-4 border-b max-sm:hidden">
-                  {job.location}
-                </td>
-                <td className="py-2 px-4 border-b text-center">
-                  {job.applicants}
-                </td>
-                <td className="py-2 px-4 border-b">
-                  <input onChange={()=>changeJobVisibility(job_id)} className="scale-125 ml-4" type="checkbox" checked={job.visible} />
-                </td>
+  return jobs ? (
+    jobs.length === 0 ? (
+      <div className="fle items-center justify-center h-[70vh]">
+        <p className="text-xl sm:text-2xl">No Jobs Available</p>
+      </div>
+    ) : (
+      <div className="container p-4 max-w-5xl">
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border-b text-left max-sm:hidden">
+                  #
+                </th>
+                <th className="px-4 py-2 border-b text-left">Job Title</th>
+                <th className="px-4 py-2 border-b text-left max-sm:hidden">
+                  Date
+                </th>
+                <th className="px-4 py-2 border-b text-left max-sm:hidden">
+                  Location
+                </th>
+                <th className="px-4 py-2 border-b text-center">Applicants</th>
+                <th className="px-4 py-2 border-b text-left">Visible</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {jobs.map((job, index) => (
+                <tr key={index} className="text-gray-700">
+                  <td className="py-2 px-4 border-b max-sm:hidden">
+                    {index + 1}
+                  </td>
+                  <td className="py-2 px-4 border-b">{job.title}</td>
+                  <td className="py-2 px-4 border-b max-sm:hidden">
+                    {moment(job.date).format("ll")}
+                  </td>
+                  <td className="py-2 px-4 border-b max-sm:hidden">
+                    {job.location}
+                  </td>
+                  <td className="py-2 px-4 border-b text-center">
+                    {job.applicants}
+                  </td>
+                  <td className="py-2 px-4 border-b">
+                    <input
+                      onChange={() => changeJobVisibility(job_id)}
+                      className="scale-125 ml-4"
+                      type="checkbox"
+                      checked={job.visible}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={() => navigate("/dashboard/add-job")}
+            className="bg-black text-white py-2 px-4 rounded cursor-pointer"
+          >
+            Add new Job
+          </button>
+        </div>
       </div>
-      <div className="mt-4 flex justify-end">
-        <button
-          onClick={() => navigate("/dashboard/add-job")}
-          className="bg-black text-white py-2 px-4 rounded cursor-pointer"
-        >
-          Add new Job
-        </button>
-      </div>
-    </div>
-  ): <Loading/>
+    )
+  ) : (
+    <Loading />
+  );
 };
 
 export default ManageJobs;
