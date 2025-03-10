339c306c3c37c9b0a3555b802ef4f6f829a606e8
MogesBekele <mogesbekele32@gmail.com>
2025-03-10 20:20:56 +0300

    commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center py-3 gap-4 mt-20'>
+    <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center py-3 gap-4 mt-20">
       <img src={assets.logo} alt="" />
-      <p className='flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @MogesBekele | All right reserved</p>
-      <div className='flex gap-2.5'>
-        <img width={38}  src={assets.facebook_icon} alt="" />
-        <img width={38}  src={assets.twitter_icon} alt="" />
+      <p className="flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
+        Copyright @MogesBekele | All right reserved
+      </p>
+      <div className="flex gap-2.5">
+        <img width={38} src={assets.facebook_icon} alt="" />
+        <img width={38} src={assets.twitter_icon} alt="" />
         <img width={38} src={assets.instagram_icon} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
