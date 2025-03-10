d2e130899831a0767d3b5dcc1baa1cc705dd5c1f
MogesBekele <mogesbekele32@gmail.com>
2025-03-10 16:16:40 +0300

    commit on useref hook

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -69,4 +69,5 @@ const Hero = () => {
 // i understand the useref application on this section
 // flex property on the input and search buttton
 // the two state value assigned on it which are setsearchfilter and setissearch 
+// ggod tounderstand state value with useref
 export default Hero;
