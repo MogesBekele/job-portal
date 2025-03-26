c1551aaae5154f1a635a4378eb4eb27d363b4d3c
MogesBekele <mogesbekele32@gmail.com>
2025-03-26 07:38:52 +0300

    commit on

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -21,7 +21,7 @@ export const AppContextProvider = (props) => {
 
   const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
 
-  const [companyToken, setCompanyToken] = useState(null);//recrieter login token
+  const [companyToken, setCompanyToken] = useState(null);//recriuter login token
   const [companyData, setCompanyData] = useState(null);
   const [userData, setUserData] = useState(null);
   const [userApplications, setUserApplications] = useState([]);
