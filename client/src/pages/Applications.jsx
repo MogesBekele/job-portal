35399c6bf5d4096c335ecc5f0df3b1a246a16cdb
MogesBekele <mogesbekele32@gmail.com>
2025-04-01 06:14:48 +0300

    commit on application component

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -199,11 +199,7 @@ export default Applications;
  // table is filterable by location
 // table is searchable by date
 
- // table is searchable by status
- // table is searchable by company
- // table is searchable by job title
- // table is searchable by location
- // table is paginated
+
  
 
 
